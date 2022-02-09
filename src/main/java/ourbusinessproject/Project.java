9909be8ccbb0db4a93f128e4e6e5b790c4aba181
Franck Silvestre <franck.silvestre@gmail.com>
2022-02-09 09:58:09 +0100

    fix #1.1 classe Project

---
+++ src/main/java/ourbusinessproject/Project.java
@@ -0,0 +1,29 @@
+package ourbusinessproject;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class Project {
+
+
+    @NotEmpty // or @NotNull @NotBlank
+    private String title;
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
