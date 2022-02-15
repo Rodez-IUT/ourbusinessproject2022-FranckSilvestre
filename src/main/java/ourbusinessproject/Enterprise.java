1ff4ceb5084cf462887fa26298b4e9fc0b6f3d6f
Franck Silvestre <franck.silvestre@gmail.com>
2022-02-15 14:57:14 +0100

    fix #1.3 Mapping Project et Enterprise

--- src/main/java/ourbusinessproject/Enterprise.java
+++ src/main/java/ourbusinessproject/Enterprise.java
@@ -1,9 +1,13 @@
 package ourbusinessproject;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+@Entity
 public class Enterprise {
 
     @NotEmpty
@@ -14,36 +18,74 @@ public class Enterprise {
     private String contactName;
     @NotEmpty @Email
     private String contactEmail;
+    @Id
+    @GeneratedValue
+    private Long id;
 
+    /**
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Set the name
+     * @param name the new name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * @return the description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Set the description
+     * @param description the new description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * @return the contact name
+     */
     public String getContactName() {
         return contactName;
     }
 
+    /**
+     * Set the contact name
+     * @param contactName the new contact name
+     */
     public void setContactName(String contactName) {
         this.contactName = contactName;
     }
 
+    /**
+     * @return the contact email
+     */
     public String getContactEmail() {
         return contactEmail;
     }
 
+    /**
+     * Set the contact email
+     * @param contactEmail the new contact email
+     */
     public void setContactEmail(String contactEmail) {
         this.contactEmail = contactEmail;
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
     }
 }
--- src/main/java/ourbusinessproject/Project.java
+++ src/main/java/ourbusinessproject/Project.java
@@ -1,24 +1,57 @@
 package ourbusinessproject;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
+
+
+/**
+ * Class representing a project
+ */
+@Entity
 public class Project {
 
     @NotEmpty // or @NotNull @NotBlank
     private String title;
     private String description;
+    @Id
+    @GeneratedValue
+    private Long id;
 
+    /**
+     * @return the title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Set the title
+     * @param title the new title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * @return the description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Set the description
+     * @param description the new description
+     */
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
     }
 }
