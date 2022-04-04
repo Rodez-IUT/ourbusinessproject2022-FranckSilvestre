7c26b18c53d4393677e59069f93367c261e845e7
Franck Silvestre <franck.silvestre@gmail.com>
2022-04-04 16:15:58 +0200

    fix #3.3 Rest Controller OK

--- src/main/java/ourbusinessproject/Enterprise.java
+++ src/main/java/ourbusinessproject/Enterprise.java
@@ -1,5 +1,7 @@
 package ourbusinessproject;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -25,7 +27,7 @@ public class Enterprise {
     @GeneratedValue
     private Long id;
 
-    @OneToMany(mappedBy = "enterprise")
+    @OneToMany(mappedBy = "enterprise") @JsonIgnore
     private Collection<Project> projects;
 
     /**
